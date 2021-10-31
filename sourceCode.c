7f4d41746c468a54ff45e64302f61003195514a5
captJakSprrw <ankitkumar3509@gmail.com>
2021-10-31 20:30:14 +0530

    added customer interface and updated required functions list inREADME

--- sourceCode.c
+++ sourceCode.c
@@ -10,7 +10,7 @@ int main()
     welcome();
     if(login())
     {
-        cond = 1;
+        int cond = 1;
         while(cond)
         {
             switch(keepermenu())
@@ -34,7 +34,24 @@ int main()
 
 void customerinterface()
 {
-    
+    int cond = 1;
+    while(cond)
+    {
+        printf("Press: \n1. For old customer.\n2. To add new customer.\n3. To return back to main menu.\n ");
+        int response;
+        scanf("%d", &response);
+        switch(response)
+        {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                cond = 0;
+                break;
+        }
+    }
 }
 
 int keepermenu()

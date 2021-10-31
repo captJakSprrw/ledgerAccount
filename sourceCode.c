977c346329f9ef58ea85e9378ff1f3403f551c7a
captJakSprrw <ankitkumar3509@gmail.com>
2021-10-31 07:00:12 +0530

    keeper interface created in source code and required functions listed in README

--- sourceCode.c
+++ sourceCode.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
+int keepermenu();
+void customerinterface();
+
+
 int main()
 {
-    printf("Hello World!!\n");
+    logoprinter();
+    welcome();
+    if(login())
+    {
+        cond = 1;
+        while(cond)
+        {
+            switch(keepermenu())
+            {
+                case 1:
+                    customerinterface();
+                    break;
+                case 2:
+                    printhistory();
+                    break;
+                case 3:
+                    printdatabase();
+                    break;
+                case 4:
+                    cond = 0;
+                    break;
+            }
+        }
+    }
+}
+
+void customerinterface()
+{
+    
+}
+
+int keepermenu()
+{
+    int response;
+    printf("Press: \n\n1. For customer menu.\n2. To print transcation history.\n3. To print all the customer's details.\n4. Quit.\n");
+    scanf("%d", &response);
+    return response;
 }

ef5f12ac9285de1176bcccc3f942858ccc9045ff
captJakSprrw <ankitkkumar3509@gmail.com>
2021-11-01 18:44:43 +0000

    completed the login() function and added the information about forgotpass() and authenticate() functions which were required to build login() in README

--- sourceCode.c
+++ sourceCode.c
@@ -7,7 +7,14 @@ int customermenu();
 void logoprinter();
 void welcome();
 int login();
+int authenticate();
+void forgotpass();
 
+struct chunk
+{
+    int len;
+    char original[15];
+};
 
 int main()
 {
@@ -131,7 +138,151 @@ void welcome()
     printf("Welcome to RecBo, a digital ledger.\n");
 }
 
+int authenticate()
+{
+    int returnval, i, trial = 1;
+    int cond = 1;
+    char buffer[15];
+    char dev[15] = "captJakSprrw";
+    while(cond)
+    {
+        printf("who is the developer of ReBo?\n");
+        scanf(" %s", buffer);
+        for(i = 0; ((buffer[i] != '\0') || (dev[i] != '\0')); i++)
+        {
+            if(buffer[i] != dev[i])
+            {
+                if(trial == 3)
+                {
+                    returnval = 0;
+                    cond = 0;
+                    break;
+                }
+                trial++;
+                break;
+            }
+            returnval = 1;
+            cond = 0;
+        }
+    }
+    return returnval;
+}
+
+void forgotpass()
+{
+    if(authenticate())
+    {
+        struct chunk originalc;
+        char buffer[15], confirmbuffer[15], temp;
+        int i,cond = 1;
+        while(cond)
+        {
+            int k;
+            printf("Enter New Password:\n");
+            scanf(" %c", &temp);
+            for(i = 0; (i < 16 && temp != '\n'); i++)
+            {
+                buffer[i] = temp;
+                scanf("%c", &temp);
+            }
+            printf("Confirm Password:\n");
+            scanf(" %c", &temp);
+            for(k = 0; (k < 16 && temp != '\n'); k++)
+            {
+                confirmbuffer[k] = temp;
+                scanf("%c", &temp);
+            }
+            if(k == i)
+            {
+                int j;
+                for(j = 0; j < i; j++)
+                {
+                    if(confirmbuffer[j] != buffer[j])
+                    {
+                        cond = 1;
+                        printf("Did not match\n");
+                        break;
+                    }
+                    originalc.original[j] = buffer[j];
+                    cond = 0;
+                }
+            }
+            else
+            {
+                printf("Did not match\n");
+            }
+        }
+        originalc.len = i;
+        FILE * fi = fopen("info", "w");
+        fwrite(&originalc, sizeof(struct chunk), 1, fi);
+        fclose(fi);
+        printf("Password changed\nPlease log in again.\n");
+    }
+}
+
 int login()
 {
-    
+    int returnval;
+    char buffer[15], temp;
+    int cond = 1, valid;
+    struct chunk info;
+    while(cond)
+    {
+        FILE * f1 = fopen("info", "r");
+        fread(&info, sizeof(struct chunk), 1, f1);
+        fclose(f1);
+        int i, response;
+        printf("Enter Password:\n");
+        scanf(" %c", &temp);
+        for(i = 0; (i < 16 && temp != '\n'); i++)
+        {
+            buffer[i] = temp;
+            scanf("%c", &temp);
+        }
+        if(info.len == i)
+        {
+            int j;
+            for(j = 0; j < i; j++)
+            {
+                if(info.original[j] != buffer[j])
+                {
+                    cond = 1;
+                    printf("incorrect password\n");
+                    printf("Press \n1. If forgot password\n2. To retry\n3.Quit\n");
+                    scanf("%d", &response);
+                    if(response == 1)
+                    {
+                        forgotpass();
+                        response = 0;
+                    }
+                    else if(response == 3)
+                    {
+                        returnval = 0;
+                        cond = 0;
+                    }
+                    break;
+                }
+                cond = 0;
+                returnval = 1;
+            }
+        }
+        else
+        {
+            printf("incorrect password\n");
+            printf("Press \n1. If forgot password\n2. To retry\n3.Quit\n");
+            scanf("%d", &response);
+            if(response == 1)
+            {
+                forgotpass();
+                response = 0;
+            }
+            else if(response == 3)
+            {
+                returnval = 0;
+                cond = 0;
+            }
+        }
+    }
+    return returnval;
 }
+

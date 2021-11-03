8b66a2357592fa112b46ca75628efe4a4eb34e1e
captJakSprrw <ankitkkumar3509@gmail.com>
2021-11-03 14:16:31 +0000

    added function editcredential(), updated info in README, the functions printdatabase() and printhistory eill be provided in version 2 . compiled the programme and errors were removed

--- sourceCode.c
+++ sourceCode.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int keepermenu();
 void customerinterface();
@@ -14,6 +15,9 @@ void addcustomer();
 void newentry(int);
 void viewhistory(int);
 int balance(int);
+void editcredential(int);
+void printhistory();
+void printdatabase();
 
 struct chunk
 {
@@ -314,64 +318,77 @@ int login()
 
 int getid()
 {
-    int cond = 1;
-    int id;
-    struct idstruct temp;
-    char inputcred[15];
-    printf("Enter mobile number : ");
-    scanf(" %s", inputcred);
-    inputcred[strcspn(inputcred, "\n")] = 0;
-    FILE * f1 = fopen("id", "r");
-    while((fread(&temp, sizeof(struct idstruct), 1, f1) == 1) && (cond == 1))
-    {
-        printf("entered while loop\n");
-        int i;
-        for(int i = 0; (inputcred[i] != '\0' && temp.credential[i] != '\0'); i++)
+        int cond = 1;
+        int id;
+        struct idstruct temp;
+        char inputcred[15];
+        printf("Enter mobile number : ");
+        scanf(" %s", inputcred);
+        inputcred[strcspn(inputcred, "\n")] = 0;
+        FILE * f1 = fopen("id", "r");
+        while((fread(&temp, sizeof(struct idstruct), 1, f1) == 1) && (cond == 1))
         {
-            printf("enterd for loop %d\n", i);
-            if(inputcred[i] != temp.credential[i])
-            {
-                printf("mismatch found\n");
-                id = 0;
-                cond = 1;
-                break;
-            }
-            printf("match found\n");
-            cond = 0;
-            id = temp.id;
+                int i;
+                for(int i = 0; (inputcred[i] != '\0' && temp.credential[i] != '\0'); i++)
+                {
+                        if(inputcred[i] != temp.credential[i])
+                        {
+                                id = 0;
+                                cond = 1;
+                                break;
+                        }
+                        cond = 0;
+                        id = temp.id;
+                }
         }
-        printf("exited for loop with cond = %d\n", cond);
-    }
-    fclose(f1);
-    return id;
+        fclose(f1);
+        return id;
 }
 
 void addcustomer()
 {
-    struct chunk idinfo, temp;
-    struct idstruct newid;
-    FILE * f1 = fopen("info", "r");
-    fseek(f1, sizeof(struct chunk), SEEK_SET);
-    fread(&idinfo, sizeof(struct chunk), 1, f1);
-    fclose(f1);
-    FILE * f2 = fopen("id", "a");
-    newid.id = (idinfo.len + 1);
-    printf("Enter mobile number to add : ");
-    scanf(" %s", newid.credential);
-    newid.credential[strcspn(newid.credential, "\n")] = 0;
-    fwrite(&newid, sizeof(struct idstruct), 1, f2);
-    fclose(f2);
-    FILE * f3 = fopen("copy", "a");
-    f1 = fopen("info", "r");
-    fread(&temp, sizeof(struct chunk), 1, f1);
-    fwrite(&temp, sizeof(struct chunk), 1, f3);
-    fseek(f3, sizeof(struct chunk), SEEK_CUR);
-    struct chunk temp2 = {newid.id, "linuxisbetter"};
-    fwrite(&temp2, sizeof(struct chunk), 1, f3);
-    fclose(f1);
-    fclose(f3);
-    remove("info");
-    rename("copy", "info");
+        struct chunk idinfo, temp;
+        struct idstruct newid;
+        FILE * f1 = fopen("info", "r");
+        fseek(f1, sizeof(struct chunk), SEEK_SET);
+        fread(&idinfo, sizeof(struct chunk), 1, f1);
+        fclose(f1);
+        FILE * f2 = fopen("id", "a");
+        newid.id = (idinfo.len + 1);
+        printf("Enter mobile number to add : ");
+        scanf(" %s", newid.credential);
+        newid.credential[strcspn(newid.credential, "\n")] = 0;
+        fwrite(&newid, sizeof(struct idstruct), 1, f2);
+        fclose(f2);
+        FILE * f3 = fopen("copy", "a");
+        f1 = fopen("info", "r");
+        fread(&temp, sizeof(struct chunk), 1, f1);
+        fwrite(&temp, sizeof(struct chunk), 1, f3);
+        fseek(f3, sizeof(struct chunk), SEEK_CUR);
+        struct chunk temp2 = {newid.id, "linuxisbetter"};
+        fwrite(&temp2, sizeof(struct chunk), 1, f3);
+        fclose(f1);
+        fclose(f3);
+        remove("info");
+        rename("copy", "info");
+        struct custinfo newinfo;
+        int j;
+        for(j = 0; newid.credential[j] != '\0'; j++)
+        {
+                newinfo.number[j] = newid.credential[j];
+        }
+        newinfo.id = newid.id;
+        printf("Enter Name : ");
+        scanf(" %[^\n]s", newinfo.name);
+        printf("Enter email address : ");
+        scanf(" %s", newinfo.email);
+        printf("Enter address : ");
+        scanf(" %[^\n]s", newinfo.address);
+        char filename[10];
+        sprintf(filename, "%d", newinfo.id);
+        FILE * f4 = fopen(filename, "w");
+        fwrite(&newinfo, sizeof(struct custinfo), 1, f4);
+        fclose(f4); 
 }
 
 void newentry(int id)
@@ -427,3 +444,52 @@ int balance(int id)
         printf("The net balance is : %d\n", sum);
 }
 
+void editcredential(int id)
+{
+	int response, i;
+	char filename[10];
+        sprintf(filename, "%d", id);
+        FILE * f1 = fopen(filename, "r");
+	FILE * f2 = fopen("infocp", "w");
+	struct custinfo curr;
+	struct transaction temp;
+	fread(&curr, sizeof(struct custinfo), 1, f1);
+	printf("Name : %s\nemail : %s\naddress : %s\nMobile No. : %s\n", curr.name, curr.email, curr.address, curr.number);
+	printf("Press :\n1. To change name\n2. To change email address\n3. To change address\n4. To return to customer menu.\n");
+	scanf("%d", &response);
+	switch(response)
+	{
+		case 1:
+			printf("New name : ");
+			scanf(" %[^\n]s", curr.name);
+			break;
+		case 2:
+			printf("New email : ");
+                        scanf(" %[^\n]s", curr.email);
+			break;
+		case 3:
+			printf("New address : ");
+                        scanf(" %[^\n]s", curr.address);
+			break;
+	}
+	fwrite(&curr, sizeof(struct custinfo), 1, f2);
+	while(fread(&temp, sizeof(struct transaction), 1, f1))
+	{
+		fwrite(&temp, sizeof(struct transaction), 1, f2);
+		fseek(f2, sizeof(struct transaction), SEEK_CUR);
+	}
+	fclose(f1);
+	fclose(f2);
+	remove(filename);
+	rename("infocp", filename);
+}
+
+void printhistory()
+{
+	printf("will be available soon\n");
+}
+
+void printdatabase()
+{
+	printf("will be available soon\n");
+}
